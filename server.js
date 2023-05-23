a949ac7697e4eadb95b18cf8f3ff62b664dc0528
DelfinaGist <fullstackcodez@gmail.com>
2023-05-23 04:33:22 -0500

    first commit new express new route

---
+++ server.js
@@ -0,0 +1,38 @@
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// View Engine Middleware Configure
+const reactViewsEngine = require('jsx-view-engine').createEngine();
+app.engine('jsx', reactViewsEngine);
+app.set('view engine', 'jsx');
+app.set('views', './views');
+
+
+//=======I.N.D.U.C.E.S.=========
+
+// Index
+app.get('/logs', (req, res) => {
+  res.send('Express is working');
+});
+
+// New
+app.get('/logs/new', (req, res) => {
+  res.render('New');
+});
+
+//DELETE
+
+//UPDATE
+
+//CREATE
+
+//EDIT
+
+//SHOW
+
+// Listen
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
---
+++ views/New.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+class New extends React.Component {
+  render() {
+    return (
+      <div>
+        <form action="/logs" method="POST">
+          <div>
+            <label htmlFor="title">Title:</label>
+            <input type="text" name="title" />
+          </div>
+          <div>
+            <label htmlFor="entry">Entry:</label>
+            <textarea name="entry"></textarea>
+          </div>
+          <div>
+            <label htmlFor="shipIsBroken">Is Ship Broken:</label>
+            <input type="checkbox" name="shipIsBroken" />
+          </div>
+          <div>
+            <input type="submit" value="Create" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default New;
