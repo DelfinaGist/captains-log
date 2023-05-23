e28c3c0652c4d633e17129de8e05b693534582a6
DelfinaGist <fullstackcodez@gmail.com>
2023-05-23 11:58:05 -0500

    Edit Route

--- views/Index.jsx
+++ views/Index.jsx
@@ -3,24 +3,28 @@ const React = require("react");
 class Index extends React.Component {
   render() {
     const { logs } = this.props;
+    console.log(logs);
     return (
       <div>
         <ul>
-          {logs.map((log, i) => {
+          {logs.map((log) => {
             const createdDate = new Date(log.createdAt);
             const formattedDate = createdDate.toLocaleString();
             return (
-              <li key={i}>
-                {log.title}
+              <li key={log._id}>
                 <a href={`logs/${log._id}`}>{log.title}</a>
                 <p>Created at: {formattedDate}</p>
+                <a href={`logs/${log._id}/edit`}> EDIT THIS LOG</a>
+                <form action={`/logs/${log._id}?_method=DELETE`} method="POST">
+                  <input type="submit" value="DELETE" />
+                </form>
               </li>
-            )
+            );
           })}
         </ul>
-        <a href={'/logs/new'}> Create new log</a>
+        <a href={'/logs/new'}>Create new log</a>
       </div>
-    )
+    );
 
   }
 
